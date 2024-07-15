a02d61e4817c07983f4a56bb3c51df0f8e7b88df
SLajuwomi <stephenlajuwomi@gmail.com>
2024-07-14 21:51:34 -0500

    Created the calculator interface

---
+++ odin-foundations-calculator/script.js
@@ -0,0 +1,34 @@
+let firstNumber = 12;
+let secondNumber = 3;
+let operator = "/";
+
+function operate(a, b, operation) {
+  switch (operation) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+console.log(operate(firstNumber, secondNumber, "/"));
